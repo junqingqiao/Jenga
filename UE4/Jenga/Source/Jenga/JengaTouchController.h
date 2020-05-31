a1ec2b1b331100327b84ed55a3621de741cb1a1d
junqingqiao <junqingqiaoustc@gmail.com>
2020-05-30 23:43:02 -0400

    Adding Android Platform

---
+++ UE4/Jenga/Source/Jenga/JengaTouchController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "JengaTouchController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JENGA_API AJengaTouchController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
